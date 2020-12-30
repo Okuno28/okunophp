d6bfbae9605ba8a1363e463b19c0ed8568c32fdb
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2020-12-30 16:36:46 +0900

    課題提出　PHP - 変数と定数

---
+++ variable/taskVariable.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskVariable</title>
+    <?php //URL : http://localhost/okunophp/variable/taskVariable.php ?>
+</head>
+<body>
+    <?php
+        const TAX = 1.1;
+        $priceApple = 110;
+        $numApple = 15;
+        $totalApple = $priceApple * $numApple;
+        $priceOrange = 80;
+        $numOrange = 12;
+        $totalOrange = $priceOrange * $numOrange;
+        $total = $totalApple + $totalOrange;
+        $totalTax = $total * TAX;
+        $totalNum = $numApple + $numOrange;
+        $oneFruit = round ($total / $totalNum, 0);
+    ?>
+    <p><?php echo('りんごの総額（税抜）は' . $totalApple . '円です。'); ?></p>
+    <p><?php echo('オレンジの総額（税抜）は' . $totalOrange . '円です。'); ?></p>
+    <p><?php echo('総額（税抜）は' . $total . '円です。'); ?></p>
+    <p><?php echo('総額（税込）は' . $totalTax . '円です。'); ?></p>
+    <p><?php echo('フルーツ1個あたりの金額（税抜）は' . $oneFruit . '円です。'); ?></p>
+</body>
+</html>
