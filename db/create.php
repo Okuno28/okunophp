ca54f39c24927266cdae9ce56956bcfc89776ff5
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2021-02-13 18:33:12 +0900

    課題提出　PHP - テーブル作成

---
+++ db/create.php
@@ -0,0 +1,27 @@
+<?php //URL : http://localhost/okunophp/db/create.php ?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>create</title>
+    </head>
+    <body>
+    <p><?php
+        echo("create table country( </br>
+            id int not null auto_increment primary key, </br>
+            name varchar(20) not null, </br>
+            area varchar(10), </br>
+            lang varchar(10) default '英語', </br>
+            number int not null default 0); </br>");
+    ?></p>
+    <p><?php
+        echo("create table heritage( </br>
+            id int not null auto_increment primary key, </br>
+            name varchar(20) not null, </br>
+            country_id int not null, </br>
+            type varchar(2) not null); </br>");
+    ?></p>
+    </body>
+</html>
