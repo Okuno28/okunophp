123c00dce253ff9a05b0773bbe9169340acbfa7e
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2021-02-18 17:49:22 +0900

    課題提出　PHP - データ削除

---
+++ db/delete.php
@@ -0,0 +1,15 @@
+<?php //URL : http://localhost/okunophp/db/delete.php ?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>create</title>
+    </head>
+    <body>
+    <p><?php
+        echo("delete from heritage where name in ('小笠原諸島', '姫路城');");
+    ?></p>
+    </body>
+</html>
